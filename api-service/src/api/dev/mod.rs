7788332cc3a79ce0c20c1ec23bb0ae4306ed989b
Kyle Smith <kyle.smith@fortisgames.com>
2023-11-30 16:28:47 -0500

    fixed reset_db routes

--- api-service/src/api/dev/mod.rs
+++ api-service/src/api/dev/mod.rs
@@ -3,5 +3,5 @@ use actix_web::web;
 mod reset_db;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    // cfg.service(web::scope("/dev/v1").service(reset_db::reset_database));
+    cfg.service(web::scope("/dev/v1").service(reset_db::reset_database));
 }
