9f73c6cc940712778bc9515d78ffb84642056431
Kyle Smith <kylkrie@gmail.com>
2024-02-26 14:31:15 -0600

    api: fixed list buildings endpoint

--- api-service/src/api/buildings/list.rs
+++ api-service/src/api/buildings/list.rs
@@ -1,8 +1,8 @@
 use crate::{
-    api::validation::{list_param::param_list_i32, validate_req_data},
+    api::validation::validate_req_data,
     error::ServiceError,
     services::postgres::{buildings::BuildingDb, DbPool},
-    utils::hashids::{decode_id_option, encode_id},
+    utils::hashids::{decode_id, decode_id_option, encode_id},
 };
 use actix_web::{get, web, HttpResponse};
 use serde::{Deserialize, Serialize};
@@ -21,9 +21,7 @@ pub struct PaginationParams {
     // TODO: validate place_id
     #[validate(length(min = 16, max = 32))]
     place_id: Option<String>,
-    #[serde(default)]
-    #[serde(deserialize_with = "param_list_i32")]
-    ids: Option<Vec<i32>>,
+    ids: Option<String>,
 }
 
 #[derive(Debug, Serialize)]
@@ -44,10 +42,18 @@ pub async fn list_buildings(
     let query = validate_req_data(query.into_inner())?;
     let offset_id = decode_id_option(&query.offset_id)?;
 
+    let ids = match query.ids {
+        Some(ids) => Some(
+            ids.split(',')
+                .map(decode_id)
+                .collect::<Result<Vec<i32>, _>>()?,
+        ),
+        None => None,
+    };
+
     // limit default to 10, max 20
     let limit = query.limit.map_or(10, |l| if l > 20 { 20 } else { l });
-    let buildings =
-        BuildingDb::list(&pool, offset_id, limit, query.place_id.clone(), query.ids).await?;
+    let buildings = BuildingDb::list(&pool, offset_id, limit, query.place_id.clone(), ids).await?;
     let next_offset_id = if buildings.len() as i32 == limit {
         buildings.last().map(|b| encode_id(b.id))
     } else {
